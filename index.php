fa04ec147b802d9782ecdb3731a08ab5e7f49680
bocharsky-bw <bocharsky.bw@gmail.com>
2017-05-01 17:58:16 +0300

    Install Twig, create a few templates with base one

--- index.php
+++ index.php
@@ -6,11 +6,25 @@ $app = new Silex\Application();
 
 $app['debug'] = true;
 
-$app->get('/', function () {
-    return 'Blog';
+// Register service providers
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+// Register routes
+$app->get('/', function () use ($app) {
+    /** @var Twig_Environment $twig */
+    $twig = $app['twig'];
+
+    return $twig->render('blog.twig');
 });
 $app->get('/blog/{id}', function ($id) use ($app) {
-    return 'Post '.$app->escape($id);
+    /** @var Twig_Environment $twig */
+    $twig = $app['twig'];
+
+    return $twig->render('blog-post.twig', [
+        'postId' => $id,
+    ]);
 });
 
 $app->run();
